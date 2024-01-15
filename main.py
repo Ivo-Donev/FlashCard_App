33d770f8c2c09cc056f8c84a5cbf18983b70d50f
Ivo Donev <153001406+Ivo-Donev@users.noreply.github.com>
2024-01-15 18:34:37 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/Greek_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="Greek", fill="black")
+    canvas.itemconfig(card_word, text=current_card["Greek"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data_2 = pandas.DataFrame(to_learn)
+    data_2.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
+window = Tk()
+window.title("Flash Card")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, command=next_card)
+unknown_button.grid(row=1, column=0)
+unknown_button.config(highlightthickness=0)
+
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, command=is_known)
+known_button.grid(row=1, column=1)
+known_button.config(highlightthickness=0)
+
+next_card()
+
+window.mainloop()
